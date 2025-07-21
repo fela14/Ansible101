deef817973029ba6f8cd2c4bae79792458d80ad2
fela14 <k.fakeye@yahoo.com>
2025-07-22 00:41:08 +0100

    Collections and Security

---
+++ chapterCollections/collections/ansible_collections/local/colors/plugins/test/blue.py
@@ -0,0 +1,21 @@
+# Ansible custom 'blue' test plugin definition.
+
+def is_blue(string):
+    blue_values = [
+        'blue',
+        '#0000ff',
+        '#00f',
+        'rgb(0,0,255)',
+        'rgb(04,04,1004)'
+    ]
+    if string in blue_values:
+        return True
+    else:
+        return False
+class TestModule(object):
+    """ Ansible blue test """
+
+    def tests(self):
+        return {
+            'blue': is_blue
+        }
